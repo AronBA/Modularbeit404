303047d14df9cb72947756780fa24e9f74af9b77
Tris10 <tristan.aeschbach@gmail.com>
2021-10-18 16:27:29 +0200

    updated

--- src/main.java
+++ src/main.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
 
 public class main {
+    public static void Startscreen(){
+        JFrame Frame = new JFrame("Crappy Bird");
+        JButton Start = new JButton("Start");
+        Frame.setSize(1500,700);
+        Frame.add(Start);
+        Frame.setVisible(true);
+
+    }
     public static void main(String[] args) {
-        public static void Frame(){
-            JFrame Frame = new JFrame("Crappy Bird");
-            Frame.setSize(1500,700);
-            Frame.setVisible(true);
-        }
+        Startscreen();
     }
 }
