0386952003e9d78ede6935eac56090899cecc03d
Tris10 <tristan.aeschbach@gmail.com>
2021-10-18 15:33:15 +0200

    updated ReadMe.md

--- src/main.java
+++ src/main.java
@@ -1,6 +1,12 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("ooIAHDIUAGSBIASUFUIASck");
+import javax.swing.*;
 
+public class main {
+    public static void Frame(){
+        JFrame Frame = new JFrame("Crappy Bird");
+        Frame.setSize(1500,700);
+        Frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        Frame();
     }
 }
