c0990ba574de620019238721851ac8e447fb476e
Tris10 <tristan.aeschbach@gmail.com>
2021-10-18 15:57:33 +0200

    updated ReadMe.md

---
+++ src/Button.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Button {
+    public static void Button(String Name) {
+        JButton Button = new JButton(Name);
+        Button.setVisible(true);
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,12 +1,11 @@
 import javax.swing.*;
 
 public class main {
-    public static void Frame(){
-        JFrame Frame = new JFrame("Crappy Bird");
-        Frame.setSize(1500,700);
-        Frame.setVisible(true);
-    }
     public static void main(String[] args) {
-        Frame();
+        public static void Frame(){
+            JFrame Frame = new JFrame("Crappy Bird");
+            Frame.setSize(1500,700);
+            Frame.setVisible(true);
+        }
     }
 }
