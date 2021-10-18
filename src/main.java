86377388b0ada9feff7d174e9761a75900e40acd
Tris10 <tristan.aeschbach@gmail.com>
2021-10-18 17:25:20 +0200

    updated

--- src/main.java
+++ src/main.java
@@ -1,16 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class main {
-    public static void Startscreen(){
-        JFrame Frame = new JFrame("Crappy Bird");
+    private void Startscreen(){
+        JPanel Startscreen = new JPanel();
+        JLabel Title = new JLabel("Crappy Bird");
         JButton Start = new JButton("Start");
-        Frame.setSize(1500,700);
-        Frame.add(Start);
-        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Frame.setVisible(true);
+        JButton Scoreboard = new JButton("Scoreboard");
+        JButton Exit = new JButton("Exit");
+        Startscreen.setLayout(new BorderLayout(20, 30));
+        Startscreen.add(Title, BorderLayout.NORTH);
+        Startscreen.add(Scoreboard, BorderLayout.WEST);
+        Startscreen.add(Start, BorderLayout.CENTER);
+        Startscreen.add(Exit, BorderLayout.EAST);
+        Frame.add(Startscreen);
 
     }
-    public static void main(String[] args) {
+    public void main(String[] args) {
+        JFrame Frame = new JFrame();
         Startscreen();
+
+        Frame.setSize(1500,700);
+        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Frame.setVisible(true);
     }
 }
